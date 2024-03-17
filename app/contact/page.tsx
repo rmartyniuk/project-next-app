6c2927e2fac8f04b91ba0e757ac3869bb6ef3a1c
Robert Martyniuk <r.martyniuk1234@gmail.com>
2024-03-17 21:11:21 +0100

    fix: delete textarea

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -56,13 +56,7 @@ export default async function ContactPage() {
                         >
                             Message
                         </label>
-                        <textarea
-                            rows="4"
-                            name="message"
-                            id="message"
-                            placeholder="Type your message"
-                            className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                        ></textarea>
+
                     </div>
                     <div>
                         <button
